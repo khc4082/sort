749df8f3b846d3148ec617a889ef20f22fb5e862
khc4082 <80371590+khc4082@users.noreply.github.com>
2021-05-04 19:08:38 +0900

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,7 +1,7 @@
 public class InsertionSort  {
     
     public static void main(String[] args) {
-        int[] arr = { 40, 10, 50, 90, 80, 30, 60 };   // 책에 있는 예시를 활용 한 수 
+        int[] arr = { 40, 10, 50, 90, 20, 80, 30, 60 };   // 책에 있는 예시를 활용 한 수 
         
         for(int i : arr) {     // for문을 이러한 방식으로 간략화 가능
             System.out.print(i + " ");
