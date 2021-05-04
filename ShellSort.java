9aa1ae7d8ddcb5ae567cec532bea7f78cb66bcb1
khc4082 <80371590+khc4082@users.noreply.github.com>
2021-05-04 14:45:58 +0900

    Update ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -1,42 +1,41 @@
 import java.util.Random;
- 
-public class Shell_Sort {
- 
+
+public class ShellSort {
     public static void main(String[] args) {
-        
-        Random rnd=new Random();
+
+        Random r=new Random();
         int size=16;
         int[] arr=new int[size];
-        
+
         System.out.printf("정렬 전 원소 (%d)개: ",size);
         for(int i=0; i<size; i++) {
-            arr[i]=rnd.nextInt(99)+1;
+            arr[i]=r.nextInt(99)+1;
             System.out.print(arr[i]+" ");
         }
         System.out.println();
-//        int[] arr = { 10, 50, 80, 90, 70 };
- 
-        shell_Sort(arr);
+//
+
+        ShellSort(arr);
     }
- 
-    private static void shell_Sort(int[] arr) {
- 
+
+    private static void ShellSort(int[] arr) {
+
         int arrSize = arr.length;
         int interval = arrSize / 2;
- 
+
         while (interval >= 1) {
             for (int i = 0; i < interval; i++) {
                 intervalSort(arr, i, arrSize - 1, interval);
- 
+
             }
             output(arr, interval);
             interval /= 2;
-            
+
         }
     }
- 
+
     private static void intervalSort(int[] arr, int start, int end, int interval) {
- 
+
         for (int i = start + interval; i <= end; i += interval) {
             int item = arr[i];
             int j = 0;
@@ -48,13 +47,13 @@ public class Shell_Sort {
             arr[j + interval] = item;
         }
     }
- 
+
     private static void output(int[] arr,int interval) {
-        
+
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        System.out.println("인터벌 : "+ interval);
+        System.out.println(" 간격 : "+ interval);
     }
 }
 
